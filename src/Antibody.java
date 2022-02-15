1bc900fe2594ec448ae2f0c2819abeff58793648
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2022-02-15 15:24:16 +0700

    Override getAttack

--- src/Antibody.java
+++ src/Antibody.java
@@ -1,4 +1,18 @@
 public class Antibody extends Host{
     int AntibodyPlaceCost ;
     int AntibodyMoveCost ;
+    public Antibody(int col, int row){
+        this.position[0] = col ;
+        this.position[1] = row ;
+    }
+    private void TurntoVirus(){
+
+    }
+    @Override
+    public void getAttack(int dmg){
+        this.HP  -= dmg ;
+        if(isDead()){
+            this.TurntoVirus();
+        }
+    }
 }
