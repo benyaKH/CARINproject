2510065498e2f5dd780009ca355d53647da18047
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2022-02-15 15:23:25 +0700

    implement isDead,getAttack

--- src/Host.java
+++ src/Host.java
@@ -4,5 +4,14 @@ public class Host {
     int MaxHP ;
     int HP ;
     int ATK ;
+    int[] position = new int[2];
+    public void Attack(Host enemy){
 
+    }
+    public void getAttack(int dmg){
+        this.HP  -= dmg ;
+    }
+    public boolean isDead(){
+        return (HP <= 0) ;
+    }
 }
