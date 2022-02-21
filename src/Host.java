32b909e3102b18e15a33fbcd24fd4cc6325baf01
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2022-02-21 16:36:47 +0700

    change methode Attack to methode shoot

--- src/Host.java
+++ src/Host.java
@@ -5,9 +5,6 @@ public class Host {
     int HP ;
     int ATK ;
     int[] position = new int[2];
-    public void Attack(Host enemy){
-
-    }
     public void getAttack(int dmg){
         this.HP  -= dmg ;
     }
