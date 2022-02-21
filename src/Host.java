570344b71d6b21a859f587a0b256876c27970540
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2022-02-21 16:31:20 +0700

    implement move

--- src/Host.java
+++ src/Host.java
@@ -11,7 +11,34 @@ public class Host {
     public void getAttack(int dmg){
         this.HP  -= dmg ;
     }
+    public void move(String direction){
+        switch (direction) {
+            case "left" -> position[0]--;
+            case "right" -> position[0]++;
+            case "down" -> position[1]++;
+            case "up" -> position[1]--;
+            case "upright" -> {
+                position[1]--;
+                position[0]++;
+            }
+            case "upleft" -> {
+                position[1]--;
+                position[1]--;
+            }
+            case "downright" -> {
+                position[1]++;
+                position[0]++;
+            }
+            case "downleft" -> {
+                position[1]++;
+                position[1]--;
+            }
+        }
+    }
     public boolean isDead(){
         return (HP <= 0) ;
+    }
+    public void shoot(String direction){
+
     }
 }
