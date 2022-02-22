b767679ec2da39d18d8aed581532c2a7d10a817b
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2022-02-22 13:18:18 +0700

    implement IfStatementNode

---
+++ src/Parser/IfStatementNode.java
@@ -0,0 +1,14 @@
+package Parser;
+
+public class IfStatementNode implements Node {
+    Node expression ;
+    Node statement ;
+    public  IfStatementNode(Node expression,Node statement){
+        this.expression =expression ;
+        this.statement = statement ;
+    }
+    @Override
+    public void prettyPrint(StringBuilder s){
+
+    }
+}
