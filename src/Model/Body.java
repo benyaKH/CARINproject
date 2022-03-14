a5bf01fb2da792b00a63ad518fc7a0e5305d9ba2
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2022-03-14 14:38:03 +0700

    add getHost

--- src/Model/Body.java
+++ src/Model/Body.java
@@ -5,34 +5,39 @@ import Parser.SyntaxError;
 import java.util.Random;
 
 public class Body {
-    int[][] map  = new int[5][5];
+    static Host[][] map  = new Host[5][5];
     int Antibodyleft ;
     int Virusleft ;
-    public void Spaw(){
-        Random rand = new Random() ;
-        int percentspaw = rand.nextInt(100);
-        System.out.println("Percent = "+percentspaw);
-        if(percentspaw<70){
-            int col = rand.nextInt(5);
-            int row = rand.nextInt(5);
-            while (true) {
-                if(map[col][row]== 0){
-                    System.out.println("(col,row) = "+col+" "+row);
-                    map[col][row]= 1 ;
-                    Virusleft++;
-                    break;
-                }else {
-                    col++;
-                }
-                System.out.println("(col,row) = "+col+" "+row);
-            }
-        }
-    }
-    public  void AddAntibody(int col,int row) throws SyntaxError {
-        if(col<6&&row<6){
-            map[col][row] = 2 ;
-            Antibodyleft++;
-        }else throw new SyntaxError("Wrong index") ;
+    // public void Spaw(){
+    //     Random rand = new Random() ;
+    //     int percentspaw = rand.nextInt(100);
+    //     System.out.println("Percent = "+percentspaw);
+    //     if(percentspaw<70){
+    //         int col = rand.nextInt(5);
+    //         int row = rand.nextInt(5);
+    //         while (true) {
+    //             if(map[col][row]== 0){
+    //                 System.out.println("(col,row) = "+col+" "+row);
+    //                 map[col][row]= 1 ;
+    //                 Virusleft++;
+    //                 break;
+    //             }else {
+    //                 col++;
+    //             }
+    //             System.out.println("(col,row) = "+col+" "+row);
+    //         }
+    //     }
+    // }
+
+    // public  void AddAntibody(int col,int row) throws SyntaxError {
+    //     if(col<6&&row<6){
+    //         map[col][row] = 2 ;
+    //         Antibodyleft++;
+    //     }else throw new SyntaxError("Wrong index") ;
+    // }
+
+    public static Host getHost(int col,int row){
+        return map[col][row] ;
     }
     //to see result in map
     public  void PrintMap(){
@@ -48,11 +53,11 @@ public class Body {
         Body A = new Body();
         A.PrintMap();
         for(int i = 0;i<10;i++){
-            A.Spaw() ;
+            // A.Spaw() ;
             A.PrintMap();
             System.out.println(A.Virusleft);
         }
-        A.AddAntibody(4,4);
+        // A.AddAntibody(4,4);
         A.PrintMap();
         System.out.println("Model.Virus = "+A.Virusleft);
         System.out.println("Model.Antibody = "+A.Antibodyleft);
