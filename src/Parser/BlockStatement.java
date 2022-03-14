5683bdf6c1b544df1b9cb9e3b343ba4e2294c1b0
PEEMTM <peem33302@gmail.com>
2022-03-14 13:05:46 +0700

    add blockstatement

--- src/Parser/BlockStatement.java
+++ src/Parser/BlockStatement.java
@@ -2,11 +2,24 @@ package Parser;
 
 import java.util.*;
 
-public class BlockStatement {
+public class BlockStatement implements Expr{
     LinkedList<Expr> prossed = new LinkedList<>();
 
-    public BlockStatement(Expr proseed){
-        this.prossed = prossed;
+    public BlockStatement(LinkedList<Expr> proseed){
+        this.prossed.addAll(proseed);
     }
 
+    @Override
+    public int eval(Map<Expr,Integer> data) {
+        Iterator itr = prossed.listIterator();
+        while(itr.hasNext()){
+            Expr statement = (Expr) itr.next();
+            statement.eval(data);
+        }
+        return 0;
+    }
+
+    @Override
+    public void prettyPrint(StringBuilder s) {
+    }
 }
