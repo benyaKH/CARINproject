45ccb364a9416ae03482bf483c0d2a504a3e96fd
PEEMTM <peem33302@gmail.com>
2022-03-14 13:08:05 +0700

    add Expr

--- src/Parser/SensorExpr.java
+++ src/Parser/SensorExpr.java
@@ -1,9 +1,10 @@
 package Parser;
 
-public class SensorExpr{
+import java.util.Map;
+
+public class SensorExpr implements Expr{
     String name;
     String direction;
-
         int ans = 0;
         int count = 0;
         int temp = 0;
@@ -17,6 +18,20 @@ public class SensorExpr{
     public SensorExpr(String name,String direction){
         this.name = name;
         this.direction = direction;
+    }
+
+    @Override
+    public int eval(Map<Expr,Integer> data){
+        switch(name){
+            case "virus" ->{}
+            case "antibody" ->{}
+            case "nearby" ->{}
+        }
+        return 0;
+    }
+
+    @Override
+    public void prettyPrint(StringBuilder s) {
     }
 
 }
