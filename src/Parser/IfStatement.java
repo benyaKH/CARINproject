a994b79ac87e836bc8b2141a34ec793ce8e5c592
PEEMTM <peem33302@gmail.com>
2022-03-14 13:06:34 +0700

    add Expr

--- src/Parser/IfStatement.java
+++ src/Parser/IfStatement.java
@@ -12,4 +12,15 @@ public class IfStatement implements Expr{
         this.true_statement = true_statement;
         this.false_statement = false_statement;
     }
+
+    @Override
+    public int eval(Map<Expr,Integer> data){
+        if(Expression.eval(data) == 0){
+            return true_statement.eval(data);
+        }else return false_statement.eval(data);
+    }
+
+    @Override
+    public void prettyPrint(StringBuilder s) {
+    }
 }
