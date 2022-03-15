94521e1845c45dfb16bc04ef6fba077898768aab
PEEMTM <peem33302@gmail.com>
2022-03-15 14:28:16 +0700

    แก้ prettyPrint(

--- src/Parser/IfStatement.java
+++ src/Parser/IfStatement.java
@@ -6,6 +6,7 @@ public class IfStatement implements Expr{
     protected Expr Expression;
     Expr true_statement;
     Expr false_statement;
+    boolean check = false;
 
     public IfStatement(Expr Expression, Expr true_statement,Expr false_statement){
         this.Expression = Expression;
@@ -16,11 +17,18 @@ public class IfStatement implements Expr{
     @Override
     public int eval(Map<Expr,Integer> data){
         if(Expression.eval(data) == 0){
+            check = true;
             return true_statement.eval(data);
         }else return false_statement.eval(data);
     }
 
     @Override
     public void prettyPrint(StringBuilder s) {
+        s.append("if");
+        Expression.prettyPrint(s);
+        if(check){
+            true_statement.prettyPrint(s);
+        }
+        else false_statement.prettyPrint(s);
     }
 }
