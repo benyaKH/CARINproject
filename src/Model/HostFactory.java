8459ec9316f028dd14f4599ebb9ff9b01bf695c6
PEEMTM <peem33302@gmail.com>
2022-03-16 20:31:21 +0700

    add genetic

--- src/Model/HostFactory.java
+++ src/Model/HostFactory.java
@@ -1,10 +1,12 @@
 package Model;
 
+import GameState.GameState;
+
 public class HostFactory {
-    public static Virus spawnVirus(int col,int row,String geneticcode){
-        return new Virus(col, row, geneticcode);
+    public static Virus spawnVirus(int col,int row,GameState gameState){
+        return new Virus(col, row,"./src/Parser/GeneticCodeVirus.txt", gameState);
     }
-    public static Antibody spawnAntibody(int col,int row,String geneticcode){
-        return new Antibody(col, row, geneticcode);
+    public static Antibody spawnAntibody(int col,int row,GameState gameState){
+        return new Antibody(col, row,"./src/Parser/GeneticCodeAntibody.txt", gameState);
     }
 }
