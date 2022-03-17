d58e49b1314862639e9179d8d647ba1437501b2e
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2022-03-17 10:29:16 +0700

    add chioce restart

--- src/GameState/BeforeFirstVirus.java
+++ src/GameState/BeforeFirstVirus.java
@@ -12,10 +12,11 @@ public class BeforeFirstVirus implements State{
     }
     public State render() throws SyntaxError{
         Scanner console = new Scanner(System.in);
+        body.Spaw();
         System.out.println("Time unit = "+ body.time);
         body.PrintMap(); 
         System.out.println("What you you want to do?");
-        System.out.println("A-Add Antibody  M-Move Antibody  S-Skip");
+        System.out.println("A-Add Antibody  M-Move Antibody  S-Skip  R-Restart");
         String choice = console.next() ;
         if(choice.equals("A")){
             System.out.println("col row");
@@ -23,9 +24,17 @@ public class BeforeFirstVirus implements State{
             int row = console.nextInt() ;
             body.AddAntibody(col, row);
         }else if(choice.equals("M")){
-            System.out.println("Which one?");
+            System.out.println("Which one?(col row)");
+            int Ocol = console.nextInt() ;
+            int Orow = console.nextInt() ;
+            System.out.println("Move to?(col row)");
+            int Ncol = console.nextInt() ;
+            int Nrow = console.nextInt() ;
+            body.MoveAntibody(Ocol, Orow, Ncol, Nrow);
         }else if(choice.equals("S")){
-
+            return  new StartState() ;
+        }else if(choice.equals("R")){
+            return  new StartState() ;
         }
         body.time++;
         if(body.getVirusleft() == 0){
--- src/GameState/Defaltstate.java
+++ src/GameState/Defaltstate.java
@@ -11,11 +11,15 @@ public class Defaltstate implements State{
         this.body = body ;
     }
     public State render() throws SyntaxError{
+        if(body.getVirusleft() == 0){
+            return new GameOverState(this.body) ;
+        }
+        body.Spaw();
         Scanner console = new Scanner(System.in);
         System.out.println("Time unit = "+ body.time);
         body.PrintMap(); 
         System.out.println("What you you want to do?");
-        System.out.println("A-Add Antibody  M-Move Antibody  S-Skip");
+        System.out.println("A-Add Antibody  M-Move Antibody  S-Skip  R-Restart");
         String choice = console.next() ;
         if(choice.equals("A")){
             System.out.println("col row");
@@ -23,9 +27,17 @@ public class Defaltstate implements State{
             int row = console.nextInt() ;
             body.AddAntibody(col, row);
         }else if(choice.equals("M")){
-            System.out.println("Which one?");
+            System.out.println("Which one?(col row)");
+            int Ocol = console.nextInt() ;
+            int Orow = console.nextInt() ;
+            System.out.println("Move to?(col row)");
+            int Ncol = console.nextInt() ;
+            int Nrow = console.nextInt() ;
+            body.MoveAntibody(Ocol, Orow, Ncol, Nrow);
         }else if(choice.equals("S")){
 
+        }else if(choice.equals("R")){
+            return  new StartState() ;
         }
         body.time++;
         return new Defaltstate(this.body) ;
